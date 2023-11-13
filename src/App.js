ac7a4348b15e3b0c77334b8b2529f20d823b4415
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2023-11-14 07:29:16 +0800

    Initialize project template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import { Fragment, useState } from 'react';
+import { UserProvider } from './UserContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* Main Navigation: Start */
+import AppNavbar from './components/AppNavbar';
+import AppFooter from './components/AppFooter';
 
-export default App;
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+/* Main Navigation: End */
+
+/* Student Navigation: Start */
+import StdntHome from './pages/student/StdntHome';
+/* Student Navigation: End */
+
+/* Teacher Navigation: Start */
+import TchrHome from './pages/teacher/TchrHome';
+/* Teacher Navigation: End */
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, far, fab);
+
+export default function App() {
+    return (
+        <Fragment>
+            <Router>
+                <AppNavbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+
+                    <Route path="/teacher/*" element={<TeacherRoutes />} />
+                    <Route path="/student/*" element={<StudentRoutes />} />
+                </Routes>
+                <AppFooter />
+            </Router>
+        </Fragment>
+    );
+};
+
+function TeacherRoutes() {
+    const [user, setUser] = useState({
+        id: null,
+        role: null,
+    });
+
+    const unsetUser = () => {
+        localStorage.clear();
+    }
+
+    return (
+        <UserProvider value={{ user, setUser, unsetUser }}>
+            <Routes>
+                <Route index element={<TchrHome />} />
+            </Routes>
+        </UserProvider>
+    );
+};
+
+function StudentRoutes() {
+    const [user, setUser] = useState({
+        id: null,
+        role: null,
+    });
+
+    const unsetUser = () => {
+        localStorage.clear();
+    }
+
+    return (
+        <UserProvider value={{ user, setUser, unsetUser }}>
+            <Routes>
+                <Route index element={<StdntHome />} />
+            </Routes>
+        </UserProvider>
+    );
+};
---
+++ src/components/teacher/TeacherNavbar.jsx
@@ -0,0 +1,6 @@
+
+export default function TeacherNavbar() {
+    return (
+        <></>
+    );
+};
