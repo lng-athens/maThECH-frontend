f587c08c8cfbd5a431bf27ce321fec0eb734663e
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2023-11-16 17:51:20 +0800

    included user auth check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import { Container } from "react-bootstrap";
+import UserContext from "../UserContext";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function Home() {
+    const { user } = useContext(UserContext);
+    
     return (
+        (user.id !== null)
+        ?
+        <Navigate to="/utility/generator" />
+        :
         <Container fluid className="page--home">
             <h3>Home Page</h3>
         </Container>
